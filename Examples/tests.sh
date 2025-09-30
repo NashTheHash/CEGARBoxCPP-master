b6ec9e7f74541d343b50dfb1afce89203325aa90
NashTheHash <nrchowdhury@icloud.com>
2025-09-30 19:27:12 +0200

    Added S5 test

--- Examples/tests.sh
+++ Examples/tests.sh
@@ -2,5 +2,6 @@
 ../kaleidoscope -f Kaxiom.txt -n
 ../kaleidoscope -f 4axiom.txt
 ../kaleidoscope -f 4axiom.txt -4
+../kaleidoscope -f S5axiom.txt -5
 #../lumen ltl.txt # infinite trace
 #../lumen -f ltl.txt # finite trace
